b41c41323159df7a8f6a860570b5e6a24c9eb273
zhoumeng0510 <1058166619@qq.com>
2017-06-14 14:37:08 +0800

    update

---
+++ src/main/java/demo/util/EncodingFilter.java
@@ -0,0 +1,31 @@
+package demo.util;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by zhoumeng on
+ * 2017.6.14.
+ * 下午 02:08.
+ */
+@WebFilter (urlPatterns = "/*")
+public class EncodingFilter implements Filter {//filter 过滤器 encoding 编码
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // initialization  初始化
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        //chain  链
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {//destroy 销毁
+
+    }
+}
